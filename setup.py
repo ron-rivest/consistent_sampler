fa588cee418486e0b6c93a14f23b85794f68bb61
ron-rivest <rivest.ron@gmail.com>
2018-08-18 19:22:01 -0700

    Modify files preparatory to making package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="consistentsampler",
+    version="1.0.3",
+    author="Ronald L. Rivest",
+    author_email="rivest@mit.edu",
+    description="Package for consistent sampling with or without replacement.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ron-rivest/consistent_sampler",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+
